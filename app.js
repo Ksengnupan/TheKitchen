c32eb5c58b3cba0ddac313667c19c812a443ab3d
sengnupan <sengnupan.kumgyi@gmail.com>
2023-03-27 18:29:40 +0700

    TheKitchenPj_initialization

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/dashboard', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
